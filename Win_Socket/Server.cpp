f6940e3448a575aba7680f23e3d9d7934a64faec
FLIPPEDz <37693122+FLIPPEDz@users.noreply.github.com>
2020-04-22 17:22:40 +0800

    Update Server.cpp

--- Win_Socket/Server.cpp
+++ Win_Socket/Server.cpp
@@ -1,10 +1,23 @@
-#define  WIN32_LEAN_AND_MEAN
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-#include <windows.h>
-#include <WinSock2.h>
+#ifdef _WIN32
+	#define  WIN32_LEAN_AND_MEAN
+	#define _WINSOCK_DEPRECATED_NO_WARNINGS
+	#include <windows.h>
+	#include <WinSock2.h>
+	#pragma comment(lib,"ws2_32.lib")
+
+#else
+	#include <unistd.h>  //unix std
+	#include <string.h>
+	#include <arpa/inet.h>
+
+	#define SOCKET unsigned int
+	#define INVALID_SOCKET  (SOCKET)(~0)
+	#define SOCKET_ERROR            (-1)
+#endif
+
 #include <stdio.h>
 #include <vector>
-//#pragma comment(lib,"ws2_32.lib")
+
 
 
 
@@ -27,7 +40,7 @@ struct Header
 };
 
 //login
-struct Login:public Header
+struct Login :public Header
 {
 	Login()
 	{
@@ -92,26 +105,26 @@ int process(SOCKET _client)
 	char recbuffer[1024] = {};
 
 
-	int len = recv(_client, recbuffer, sizeof(Header), 0);
+	int len = (int)recv(_client, recbuffer, sizeof(Header), 0);
 
 	Header* head = (Header*)recbuffer;
 
 
 	if (len <= 0)
 	{
-		printf("<client:%d> quit task!\n",_client);
+		printf("<client:%d> quit task!\n", _client);
 		return -1;
 	}
-	
+
 	switch (head->cmd)
 	{
 	case CMD_LOGIN:
 	{
 
-		recv(_client, recbuffer + sizeof(head), head->lendata - sizeof(head), 0);
+		recv(_client, recbuffer + sizeof(Header), head->lendata - sizeof(Header), 0);
 
 		Login* login = (Login*)recbuffer;
-		printf("recv <client:%d> cmd:CMD_LOGIN datalen:%d username:%s userpsw:%s\n",_client, login->lendata
+		printf("recv <client:%d> cmd:CMD_LOGIN datalen:%d username:%s ,userpsw:%s \n", _client, login->lendata
 			, login->usrname, login->password);
 		//忽略判断用户密码是否正确
 
@@ -122,9 +135,9 @@ int process(SOCKET _client)
 	case CMD_LOGINOUT:
 	{
 
-		recv(_client, recbuffer + sizeof(head), head->lendata - sizeof(head), 0);
+		recv(_client, recbuffer + sizeof(Header), head->lendata - sizeof(Header), 0);
 		LoginOut* loginout = (LoginOut*)recbuffer;
-		printf("recv <client:%d> cmd:CMD_LOGINOUT datalen:%d username:%s\n",_client, loginout->lendata
+		printf("recv <client:%d> cmd:CMD_LOGINOUT datalen:%d username:%s \n", _client, loginout->lendata
 			, loginout->usrname);
 
 		//忽略判断用户密码是否正确
@@ -136,7 +149,7 @@ int process(SOCKET _client)
 	break;
 	default:
 	{
-		Header he = { 0,CMD_ERROR };
+		Header head = { 0,CMD_ERROR };
 		send(_client, (char*)&head, sizeof(head), 0);
 	}
 	break;
@@ -147,22 +160,25 @@ int process(SOCKET _client)
 
 int main()
 {
+#ifdef _WIN32  
 	WORD var = MAKEWORD(2, 2);
 	WSADATA data;
-
 	WSAStartup(var, &data);
-
+#endif
 	SOCKET _sock;
-	_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	_sock = socket(AF_INET, SOCK_STREAM, 0);
 
 	sockaddr_in _sin = {};
 
 	_sin.sin_family = AF_INET;
 	_sin.sin_port = htons(40000);
+#ifdef _WIN32
 	_sin.sin_addr.S_un.S_addr = INADDR_ANY;
+#else
+	_sin.sin_addr.s_addr = INADDR_ANY;
+#endif
 
-
-	if (SOCKET_ERROR==bind(_sock, (sockaddr*)&_sin, sizeof(_sin)))
+	if (SOCKET_ERROR == bind(_sock, (sockaddr*)&_sin, sizeof(sockaddr_in)))
 	{
 		printf("bind fault!\n");
 	}
@@ -188,7 +204,7 @@ int main()
 		fd_set fd_read;
 		fd_set fd_write;
 		fd_set fd_exp;
-		
+
 		FD_ZERO(&fd_read);
 		FD_ZERO(&fd_write);
 		FD_ZERO(&fd_exp);
@@ -198,25 +214,32 @@ int main()
 		FD_SET(_sock, &fd_write);
 		FD_SET(_sock, &fd_exp);
 
-		for (int i=(int)g_clients.size()-1;i>=0;i--)
+		SOCKET maxsock = _sock;
+
+		for (int i = (int)g_clients.size() - 1;i >= 0;i--)
 		{
 			FD_SET(g_clients[i], &fd_read);
+			if (maxsock < g_clients[i])
+			{
+				/* code */
+				maxsock = g_clients[i];
+			}
 		}
 
-		timeval ti_me{1,0};
-		
+		timeval ti_me{ 1,0 };
+
 
 		//nfds 整数值 指集合中所有描述符(socket)的范围，而不是数量
 		//windows中可以写零
-		int ret=select(_sock+1,&fd_read,&fd_write,&fd_exp,&ti_me);
+		int ret = select(maxsock + 1, &fd_read, &fd_write, &fd_exp, &ti_me);
 
-		if (ret<0)
+		if (ret < 0)
 		{
 			printf("select quit task!\n");
 			break;
 		}
-		
-		if (FD_ISSET(_sock,&fd_read))
+
+		if (FD_ISSET(_sock, &fd_read))
 		{
 			FD_CLR(_sock, &fd_read);
 			sockaddr_in clientAddr = {};
@@ -224,9 +247,11 @@ int main()
 			SOCKET _client = INVALID_SOCKET;
 
 			int len = sizeof(clientAddr);
-
+#ifdef _WIN32
 			_client = accept(_sock, (sockaddr*)&clientAddr, &len);
-
+#else            
+			_client = accept(_sock, (sockaddr*)&clientAddr, (socklen_t *)&len);
+#endif
 			if (INVALID_SOCKET == _client)
 			{
 				printf("accept _client fault!\n");
@@ -242,34 +267,48 @@ int main()
 				g_clients.push_back(_client);
 
 				printf("new client socket=%d ip=%s\n", (int)_client, inet_ntoa(clientAddr.sin_addr));
-			}		
-			
+			}
+
 		}
 
-		for (size_t i=0;i<fd_read.fd_count;++i)
+		for (int i = (int)g_clients.size() - 1;i >= 0;i--)
 		{
-			if (process(fd_read.fd_array[i])==-1)
+			if (FD_ISSET(g_clients[i], &fd_read))
 			{
-				auto itr = find(g_clients.begin(), g_clients.end(), g_clients[i]);
-				if (itr!=g_clients.end())
+				if (process(g_clients[i]) == -1)
 				{
-					g_clients.erase(itr);
+					auto itr = g_clients.begin()+i;
+					if (itr != g_clients.end())
+					{
+						g_clients.erase(itr);
+					}
 				}
-			}
-		}	
 
-		printf("空闲处理其他任务\n");
+			}
+
+		}
+
+		//printf("空闲处理其他任务\n");
 	}
 
-
+#ifdef _WIN32
 	for (size_t n = 0;n < g_clients.size();++n)
 	{
 		closesocket(g_clients[n]);
 	}
-	
-	g_clients.clear();
-
+	closesocket(_sock);
 	WSACleanup();
+
+#else
+	for (size_t n = 0;n < g_clients.size();++n)
+	{
+		close(g_clients[n]);
+	}
+	close(_sock);
+
+#endif
+
+	g_clients.clear();
 	printf("finish task quit!\n");
 
 	getchar();
